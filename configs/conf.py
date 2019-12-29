95cf94257ef7a660c53af6c2a7e0ff300c6954b0
nrshka <retributioniswar@gmail.com>
2019-12-29 19:00:28 +0700

    add flags defnes metafile existing

--- configs/conf.py
+++ configs/conf.py
@@ -3,6 +3,8 @@ from enum import Enum
 ErrCodes = Enum('ErrCodes', 'WRONG_ID WRONG_CODE NO_CODE NO_ID WRONG_PACKET')
 ReqCodes = Enum('ReqCodes', 'GET_ID NEW_IMAGES SUCCESS GET_NN_PREDICTION')
 
+saveMetafiles: bool = False
+
 client_config = {
     'url': "http://localhost:4000/jsonrpc",
     'headers': {'content-type': 'application/json'}
